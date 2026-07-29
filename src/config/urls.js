60a2323bc1c8a4c7db2fbcfe7d1dee63eba710c6
Yohannes mitike <jonmersha@gmail.com>
2026-07-29 09:33:50 +0300

    parking

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,2 +1,2 @@
-// export const ERP_BACKEND_URL = 'https://milkitest.besheger.com/api/erp-users';
-export const ERP_BACKEND_URL = 'http://localhost:4000/api/erp-users';
+export const ERP_BACKEND_URL = 'https://milkitest.besheger.com/api/erp-users';
+// export const ERP_BACKEND_URL = 'http://localhost:4000/api/erp-users';
